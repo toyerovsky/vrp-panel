a7d66978ba9929e2926e81bfb00e4680c63b217c
Cherro <kaliszewski.dominik@gmail.com>
2018-07-04 00:35:27 +0200

    Add router, angular materials and Toastr notification

--- vrp-panels/src/app/app.component.ts
+++ vrp-panels/src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  constructor(private _notification: ToastrService){}
+
+  ngOnInit(){
+    setTimeout(() => this._notification.error("Test","Tytuł test"));
+  }
 }
--- vrp-panels/src/app/app.module.ts
+++ vrp-panels/src/app/app.module.ts
@@ -2,13 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './/app-routing.module';
+import { ToastrModule } from 'ngx-toastr';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    ToastrModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
