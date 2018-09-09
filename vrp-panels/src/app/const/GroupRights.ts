56c3a2e0aaa3f53693f1a243fb8cb8d2d10056b5
Przemysław P <toyerek@gmail.com>
2018-09-09 12:22:44 +0200

    Add template for group rights, change indents in Misc.ts

--- vrp-panels/src/app/const/GroupRights.ts
+++ vrp-panels/src/app/const/GroupRights.ts
@@ -6,7 +6,40 @@ export interface GroupRights {
 export interface GroupRight {
   label: string;
   field: string;
+  value: number;
 }
 
 export const GROUP_RIGHTS: GroupRights[] = [
+  {
+    groupType: 'Taxi',
+    rights: []
+  },
+  {
+    groupType: 'Bar',
+    rights: []
+  },
+  {
+    groupType: 'Klub',
+    rights: []
+  },
+  {
+    groupType: 'Organizacja przestępcza',
+    rights: []
+  },
+  {
+    groupType: 'Warsztat',
+    rights: []
+  },
+  {
+    groupType: 'Policja',
+    rights: []
+  },
+  {
+    groupType: 'Szpital',
+    rights: []
+  },
+  {
+    groupType: 'Wiadomości',
+    rights: []
+  }
 ];
